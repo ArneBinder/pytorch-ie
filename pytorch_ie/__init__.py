96d62dcbda84881aeb499a775572c7a97eb16c63
Christoph Alt <christoph.alt@posteo.de>
2022-01-02 22:49:20 +0100

    Add imports to module inits

--- pytorch_ie/__init__.py
+++ pytorch_ie/__init__.py
@@ -1,1 +1,11 @@
-"""State-of-the-art Natural Language Processing"""
+from pytorch_ie.data.document import Document
+from pytorch_ie import taskmodules
+from pytorch_ie import models
+from pytorch_ie.pipeline import Pipeline
+
+__all__ = [
+    "taskmodules",
+    "models",
+    "Document",
+    "Pipeline",
+]
---
+++ pytorch_ie/models/__init__.py
@@ -0,0 +1,9 @@
+from pytorch_ie.models.transformer_span_classification import TransformerSpanClassificationModel
+from pytorch_ie.models.transformer_text_classification import TransformerTextClassificationModel
+from pytorch_ie.models.transformer_token_classification import TransformerTokenClassificationModel
+
+__all__ = [
+    "TransformerSpanClassificationModel",
+    "TransformerTextClassificationModel",
+    "TransformerTokenClassificationModel",
+]
