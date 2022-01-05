2954637bab4979ccdc434d5a6236de5e79683b58
Arne Binder <arne.binder@dfki.de>
2022-01-05 01:37:32 +0100

    generalize annotate_dataset_with_sections to annotate_dataset_with_partitions

--- pytorch_ie/data/datamodules/sciarg_datamodule.py
+++ pytorch_ie/data/datamodules/sciarg_datamodule.py
@@ -1,17 +1,24 @@
+from functools import partial
 from typing import Optional
 
 from pytorch_ie.data.datamodules.datamodule import DataModule
-from pytorch_ie.data.annotation_utils import annotate_dataset_with_sections
+from pytorch_ie.data.annotation_utils import annotate_dataset_with_partitions
 from pytorch_ie.data.datasets.brat import load_brat
 
 
 class SciargDataModule(DataModule):
-
     def __init__(self, **kwargs):
         super().__init__(
             data_config_path='./pytorch_ie/data/datasets/hf_datasets/sciarg.json',
             load_data=load_brat,
-            dataset_preprocessing_hook=annotate_dataset_with_sections,
+            dataset_preprocessing_hook=partial(
+                annotate_dataset_with_partitions,
+                annotation_name="sections",
+                label_whitelist=["Title", "Abstract", "H1"],
+                split_pattern=r"<([^>/]+)>.*</\1>",
+                label_group_id=1,
+                #skip_initial_partition=True,
+            ),
             **kwargs
         )
 
