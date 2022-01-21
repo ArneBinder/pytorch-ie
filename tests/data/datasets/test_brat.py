1b1eb9ef8ec50cacde728804f7a0b75064f7166e
Arne Binder <arne.binder@dfki.de>
2022-01-21 18:11:39 +0100

    fix and adapt tests for load/serialize brat

--- tests/data/datasets/test_brat.py
+++ tests/data/datasets/test_brat.py
@@ -1,82 +1,107 @@
 import os
+from typing import Dict, List
 
 from datasets import GenerateMode, set_caching_enabled
 
+from pytorch_ie import Document
+from pytorch_ie.data import BinaryRelation, LabeledSpan
 from pytorch_ie.data.datasets.brat import load_brat, serialize_brat
 from tests import FIXTURES_ROOT
 
 TEXT_01 = "Jane lives in Berlin.\n"
 TEXT_02 = "Seattle is a rainy city. Jenny Durkan is the city's mayor.\n"
-ANNOTS_01 = [
-    "T1\tperson 0 4\tJane",
-    "T2\tcity 14 20\tBerlin",
+ANNOTS_01_GENERATED_IDS = ["T3da2b611\tperson 0 4\tJane\n", "T9f2f8b43\tcity 14 20\tBerlin\n"]
+ANNOTS_02_GENERATED_IDS = [
+    "T3eff2f7a\tcity 0 7\tSeattle\n",
+    "T9ddf5dd1\tperson 25 37\tJenny Durkan\n",
+    "Rb7123b8e\tmayor_of head:T9ddf5dd1 tail:T3eff2f7a\n",
 ]
-ANNOTS_02 = [
-    "T1\tcity 0 7\tSeattle",
-    "T2\tperson 25 37\tJenny Durkan",
-    "R1\tmayor_of head:T2 tail:T1",
+ANNOTS_01_SPECIFIED_IDS = [
+    "T1\tperson 0 4\tJane\n",
+    "T2\tcity 14 20\tBerlin\n",
 ]
+ANNOTS_02_SPECIFIED_IDS = [
+    "T1\tcity 0 7\tSeattle\n",
+    "T2\tperson 25 37\tJenny Durkan\n",
+    "R1\tmayor_of head:T2 tail:T1\n",
+]
+
+
+def get_doc1(entity_annotation_name: str, with_ids: bool = False, **kwargs) -> Document:
+    doc = Document(text=TEXT_01, doc_id="1" if with_ids else None)
+    ent1 = LabeledSpan(start=0, end=4, label="person")
+    ent2 = LabeledSpan(start=14, end=20, label="city")
+    ent1.metadata["text"] = doc.text[ent1.start : ent1.end]
+    ent2.metadata["text"] = doc.text[ent2.start : ent2.end]
+    doc.add_annotation(name=entity_annotation_name, annotation=ent1)
+    doc.add_annotation(name=entity_annotation_name, annotation=ent2)
+    if with_ids:
+        ent1.metadata["id"] = "1"
+        ent2.metadata["id"] = "2"
+    return doc
+
+
+def get_doc2(
+    entity_annotation_name: str, relation_annotation_name: str, with_ids: bool = False
+) -> Document:
+    doc = Document(text=TEXT_02, doc_id="2" if with_ids else None)
+    ent1 = LabeledSpan(start=0, end=7, label="city")
+    ent2 = LabeledSpan(start=25, end=37, label="person")
+    ent1.metadata["text"] = doc.text[ent1.start : ent1.end]
+    ent2.metadata["text"] = doc.text[ent2.start : ent2.end]
+    doc.add_annotation(name=entity_annotation_name, annotation=ent1)
+    doc.add_annotation(name=entity_annotation_name, annotation=ent2)
+    rel1 = BinaryRelation(head=ent2, tail=ent1, label="mayor_of")
+    doc.add_annotation(name=relation_annotation_name, annotation=rel1)
+    if with_ids:
+        ent1.metadata["id"] = "1"
+        ent2.metadata["id"] = "2"
+        rel1.metadata["id"] = "1"
+    return doc
+
+
+def get_dataset(split_name: str = "train", with_ids: bool = False) -> Dict[str, List[Document]]:
+    entity_annotation_name = "entities"
+    relation_annotation_name = "relations"
+    dataset = {
+        split_name: [
+            get_doc1(
+                entity_annotation_name=entity_annotation_name,
+                relation_annotation_name=relation_annotation_name,
+                with_ids=with_ids,
+            ),
+            get_doc2(
+                entity_annotation_name=entity_annotation_name,
+                relation_annotation_name=relation_annotation_name,
+                with_ids=with_ids,
+            ),
+        ]
+    }
+    return dataset
+
+
+def assert_dataset_equal(dataset, other):
+    # assert that datasets (i.e. lists of documents) are equal
+    assert dataset.keys() == other.keys()
+    for split in dataset:
+        assert len(dataset[split]) == len(other[split]), f"length mismatch for split: {split}"
+        for doc, doc_loaded in zip(dataset[split], other[split]):
+            # for now, just compare string representations
+            assert str(doc) == str(doc_loaded)
 
 
 def test_load_brat():
+    dataset = get_dataset(with_ids=True)
     set_caching_enabled(False)
-    dataset = load_brat(
+    dataset_loaded = load_brat(
         url=os.path.join(FIXTURES_ROOT, "datasets/brat"),
         conversion_kwargs=dict(head_argument_name="head", tail_argument_name="tail"),
         download_mode=GenerateMode.FORCE_REDOWNLOAD,
     )
-
-    assert isinstance(dataset, dict)
-    assert len(dataset) == 1
-    assert "train" in dataset
-    documents = dataset["train"]
-    assert len(documents) == 2
-
-    document = documents[0]
-    assert document.text == TEXT_01
-
-    entities = document.annotations("entities")
-    assert len(entities) == 2
-
-    entity = entities[0]
-    assert entity.start == 0
-    assert entity.end == 4
-    assert entity.label == "person"
-    assert document.text[entity.start : entity.end] == "Jane"
-
-    entity = entities[1]
-    assert entity.start == 14
-    assert entity.end == 20
-    assert entity.label == "city"
-    assert document.text[entity.start : entity.end] == "Berlin"
-
-    document = documents[1]
-    assert document.text == TEXT_02
-
-    entities = document.annotations("entities")
-    assert len(entities) == 2
-
-    entity = entities[0]
-    assert entity.start == 0
-    assert entity.end == 7
-    assert entity.label == "city"
-    assert document.text[entity.start : entity.end] == "Seattle"
-
-    entity = entities[1]
-    assert entity.start == 25
-    assert entity.end == 37
-    assert entity.label == "person"
-    assert document.text[entity.start : entity.end] == "Jenny Durkan"
-
-    relations = document.annotations("relations")
-    assert len(relations) == 1
-    relation = relations[0]
-    assert relation.label == "mayor_of"
-    assert relation.head == entities[1]
-    assert relation.tail == entities[0]
+    assert_dataset_equal(dataset, dataset_loaded)
 
 
-def test_serialize_brat_in_memory():
+def test_load_and_serialize_brat_in_memory():
     head_argument_name = "head"
     tail_argument_name = "tail"
     set_caching_enabled(False)
@@ -97,53 +122,74 @@ def test_serialize_brat_in_memory():
     assert len(serialized_docs) == 2
 
     serialized_doc = serialized_docs[0]
-    assert serialized_doc[0] == "01"
+    assert serialized_doc[0] == "1"
     assert serialized_doc[1] == TEXT_01
-    assert serialized_doc[2] == ANNOTS_01
+    assert serialized_doc[2] == ANNOTS_01_SPECIFIED_IDS
 
     serialized_doc = serialized_docs[1]
-    assert serialized_doc[0] == "02"
+    assert serialized_doc[0] == "2"
     assert serialized_doc[1] == TEXT_02
-    assert serialized_doc[2] == ANNOTS_02
+    assert serialized_doc[2] == ANNOTS_02_SPECIFIED_IDS
+
+
+def test_serialize_brat_with_construct_ids_in_memory():
+    head_argument_name = "head"
+    tail_argument_name = "tail"
+
+    dataset = get_dataset(with_ids=False)
+    serialized_brat = serialize_brat(
+        dataset,
+        head_argument_name=head_argument_name,
+        tail_argument_name=tail_argument_name,
+    )
+    assert len(serialized_brat) == 1
+    assert "train" in serialized_brat
+    serialized_docs = list(serialized_brat["train"])
+    assert len(serialized_docs) == 2
+
+    serialized_doc = serialized_docs[0]
+    assert serialized_doc[0] is None
+    assert serialized_doc[1] == TEXT_01
+    assert serialized_doc[2] == ANNOTS_01_GENERATED_IDS
+
+    serialized_doc = serialized_docs[1]
+    assert serialized_doc[0] is None
+    assert serialized_doc[1] == TEXT_02
+    assert serialized_doc[2] == ANNOTS_02_GENERATED_IDS
 
 
 def test_serialize_brat_to_directories(tmp_path):
+
+    dataset = get_dataset(with_ids=False)
+
     head_argument_name = "head"
     tail_argument_name = "tail"
-
-    set_caching_enabled(False)
-    dataset = load_brat(
-        url=os.path.join(FIXTURES_ROOT, "datasets/brat"),
-        conversion_kwargs=dict(
-            head_argument_name=head_argument_name, tail_argument_name=tail_argument_name
-        ),
-        download_mode=GenerateMode.FORCE_REDOWNLOAD,
-    )
-
     serialize_brat(
         dataset,
         path=str(tmp_path),
         head_argument_name=head_argument_name,
         tail_argument_name=tail_argument_name,
     )
-    with open(tmp_path / "train/01.txt") as f_text:
+    doc1_id = "c56d9dd8"
+    with open(tmp_path / f"train/{doc1_id}.txt") as f_text:
         text_01 = "".join(f_text.readlines())
     assert text_01 == TEXT_01
 
-    with open(tmp_path / "train/01.ann") as f_text:
+    with open(tmp_path / f"train/{doc1_id}.ann") as f_text:
         annots_01 = f_text.readlines()
-    assert annots_01 == [f"{ann}\n" for ann in ANNOTS_01]
+    assert annots_01 == ANNOTS_01_GENERATED_IDS
 
-    with open(tmp_path / "train/02.txt") as f_text:
+    doc2_id = "a92df0b9"
+    with open(tmp_path / f"train/{doc2_id}.txt") as f_text:
         text_02 = "".join(f_text.readlines())
     assert text_02 == TEXT_02
 
-    with open(tmp_path / "train/02.ann") as f_text:
+    with open(tmp_path / f"train/{doc2_id}.ann") as f_text:
         annots_02 = f_text.readlines()
-    assert annots_02 == [f"{ann}\n" for ann in ANNOTS_02]
+    assert annots_02 == ANNOTS_02_GENERATED_IDS
 
 
-def test_full_cycle(tmp_path):
+def test_load_and_serialize_and_load_brat(tmp_path):
     head_argument_name = "head"
     tail_argument_name = "tail"
 
@@ -162,7 +208,7 @@ def test_full_cycle(tmp_path):
         tail_argument_name=tail_argument_name,
     )
     set_caching_enabled(False)
-    dataset_reloaded = load_brat(
+    dataset_loaded = load_brat(
         url=str(tmp_path),
         conversion_kwargs=dict(
             head_argument_name=head_argument_name, tail_argument_name=tail_argument_name
@@ -170,12 +216,4 @@ def test_full_cycle(tmp_path):
         download_mode=GenerateMode.FORCE_REDOWNLOAD,
     )
 
-    # assert that datasets (i.e. lists of documents) are equal
-    assert dataset.keys() == dataset_reloaded.keys()
-    for split in dataset:
-        assert len(dataset[split]) == len(
-            dataset_reloaded[split]
-        ), f"length mismatch for split: {split}"
-        for doc, doc_loaded in zip(dataset[split], dataset_reloaded[split]):
-            # for now, just compare string representations
-            assert str(doc) == str(doc_loaded)
+    assert_dataset_equal(dataset, dataset_loaded)
