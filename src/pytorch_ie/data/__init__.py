e961cc3248ec372daeb184d908a928ade0106990
Arne Binder <arne.binder@dfki.de>
2022-03-11 16:28:20 +0100

    add DataModule and simple DatasetDict to data package

--- src/pytorch_ie/data/__init__.py
+++ src/pytorch_ie/data/__init__.py
@@ -1,10 +1,14 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
+from pytorch_ie.data.datamodules.datamodule import DataModule
 from pytorch_ie.data.document import BinaryRelation, Document, LabeledSpan
 
 Metadata = Dict[str, Any]
+DatasetDict = Dict[str, List[Document]]
 
 __all__ = [
+    "DataModule",
+    "DatasetDict",
     "Document",
     "LabeledSpan",
     "BinaryRelation",
