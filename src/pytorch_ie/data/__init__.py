5d7e2acea36be85210b43641c0d6d59104f29ce2
Arne Binder <arne.binder@dfki.de>
2022-03-11 17:16:03 +0100

    remove DataModule from data package to prevent circular import

--- src/pytorch_ie/data/__init__.py
+++ src/pytorch_ie/data/__init__.py
@@ -1,13 +1,11 @@
 from typing import Any, Dict, List
 
-from pytorch_ie.data.datamodules.datamodule import DataModule
 from pytorch_ie.data.document import BinaryRelation, Document, LabeledSpan
 
 Metadata = Dict[str, Any]
 DatasetDict = Dict[str, List[Document]]
 
 __all__ = [
-    "DataModule",
     "DatasetDict",
     "Document",
     "LabeledSpan",
