aa746b40fe5d4951da54b0fe045ef256df1a3771
Arne Binder <arne.binder@dfki.de>
2022-03-13 19:42:39 +0100

    create and use PIEDatasetDict instead of DatasetDict

--- src/pytorch_ie/data/__init__.py
+++ src/pytorch_ie/data/__init__.py
@@ -3,12 +3,10 @@ from typing import Any, Dict, List
 from pytorch_ie.data.document import BinaryRelation, Document, LabeledSpan
 
 Metadata = Dict[str, Any]
-DatasetDict = Dict[str, List[Document]]
 
 __all__ = [
     # TODO: this should also directly export "DataModule",
     #  but this causes a cyclic import for now
-    "DatasetDict",
     "Document",
     "LabeledSpan",
     "BinaryRelation",
