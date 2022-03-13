0ae420d508cd4793d80880960ce415f65a9d7fe2
Arne Binder <arne.binder@dfki.de>
2022-03-14 00:28:12 +0100

    outsource _hf_example_to_document and further renaming

--- src/pytorch_ie/data/datasets/conll2003.py
+++ src/pytorch_ie/data/datasets/conll2003.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Union
+from typing import Any, Callable, Dict, List, Optional, Union
 
 from datasets import Dataset, DatasetDict, IterableDataset, IterableDatasetDict, load_dataset
 from datasets.splits import Split
@@ -32,51 +32,57 @@ def load_hf_conll2003(
     return data
 
 
-def _convert_hf_conll2003_to_document_dataset(
+def _hf_example_to_document(
+    example: Dict[str, Any], int_to_str: Callable[[int], str], entity_layer: str = "entities"
+) -> Document:
+    tokens = example["tokens"]
+    ner_tags = example["ner_tags"]
+
+    start = 0
+    token_offsets = []
+    tag_sequence = []
+    for token, tag_id in zip(tokens, ner_tags):
+        end = start + len(token)
+        token_offsets.append((start, end))
+        tag_sequence.append(int_to_str(tag_id))
+
+        start = end + 1
+
+    text = " ".join(tokens)
+    spans = bio_tags_to_spans(tag_sequence)
+
+    document = Document(text)
+    document.annotations.spans.create_layer(name=entity_layer)
+
+    for label, (start, end) in spans:
+        start_offset = token_offsets[start][0]
+        end_offset = token_offsets[end][1]
+        document.add_annotation(
+            entity_layer, LabeledSpan(start=start_offset, end=end_offset, label=label)
+        )
+    return document
+
+
+def _hf_to_document_dataset(
     hf_dataset: Union[Dataset, IterableDataset],
     entity_layer: str = "entities",
 ) -> List[Document]:
     int_to_str = hf_dataset.features["ner_tags"].feature.int2str
-
     documents = []
     for example in hf_dataset:
-        tokens = example["tokens"]
-        ner_tags = example["ner_tags"]
-
-        start = 0
-        token_offsets = []
-        tag_sequence = []
-        for token, tag_id in zip(tokens, ner_tags):
-            end = start + len(token)
-            token_offsets.append((start, end))
-            tag_sequence.append(int_to_str(tag_id))
-
-            start = end + 1
-
-        text = " ".join(tokens)
-        spans = bio_tags_to_spans(tag_sequence)
-
-        document = Document(text)
-        document.annotations.spans.create_layer(name=entity_layer)
-
-        for label, (start, end) in spans:
-            start_offset = token_offsets[start][0]
-            end_offset = token_offsets[end][1]
-            document.add_annotation(
-                entity_layer, LabeledSpan(start=start_offset, end=end_offset, label=label)
-            )
-
+        document = _hf_example_to_document(
+            example=example, int_to_str=int_to_str, entity_layer=entity_layer
+        )
         documents.append(document)
     return documents
 
 
-def convert_hf_conll2003_to_document_dataset(
-    hf_dataset: Union[DatasetDict, IterableDatasetDict],
+def from_hf(
+    hf_dataset_dict: Union[DatasetDict, IterableDatasetDict],
     entity_layer: str = "entities",
 ) -> PIEDatasetDict:
     return {
-        k: _convert_hf_conll2003_to_document_dataset(v, entity_layer=entity_layer)
-        for k, v in hf_dataset.items()
+        k: _hf_to_document_dataset(v, entity_layer=entity_layer) for k, v in hf_dataset_dict.items()
     }
 
 
@@ -85,6 +91,6 @@ def load_conll2003(
     split: Union[str, Split],
 ) -> List[Document]:
     dataset_hf = load_hf_conll2003(split=split)
-    dataset = convert_hf_conll2003_to_document_dataset(hf_dataset=dataset_hf)
+    dataset = from_hf(hf_dataset_dict=dataset_hf)
 
     return dataset[split]
