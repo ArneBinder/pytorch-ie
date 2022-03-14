6fa5d081898b5d30db635b02c7eb64b0bdbf88a3
Arne Binder <arne.binder@dfki.de>
2022-03-14 12:55:43 +0100

    revert data.__init__ to state of main branch to decrease noise

--- src/pytorch_ie/data/__init__.py
+++ src/pytorch_ie/data/__init__.py
@@ -1,12 +1,10 @@
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 from pytorch_ie.data.document import BinaryRelation, Document, LabeledSpan
 
 Metadata = Dict[str, Any]
 
 __all__ = [
-    # TODO: this should also directly export "DataModule",
-    #  but this causes a cyclic import for now
     "Document",
     "LabeledSpan",
     "BinaryRelation",
