67b763b9cd1857b7dc448c2a5c4dfdba6fc462bd
Christoph Alt <christoph.alt@posteo.de>
2022-05-02 11:44:31 +0200

    Add GermaNER dataset

---
+++ datasets/germaner/germaner.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+import datasets
+import pytorch_ie.data.builder
+from pytorch_ie import AnnotationList, LabeledSpan, TextDocument, annotation_field
+from pytorch_ie.utils.span import tokens_and_tags_to_text_and_labeled_spans
+
+
+@dataclass
+class GermaNERDocument(TextDocument):
+    entities: AnnotationList[LabeledSpan] = annotation_field(target="text")
+
+
+class GermaNER(pytorch_ie.data.builder.GeneratorBasedBuilder):
+    DOCUMENT_TYPE = GermaNERDocument
+
+    BASE_DATASET_PATH = "germaner"
+
+    VERSION = datasets.Version("0.9.1")
+
+    def _generate_document_kwargs(self, dataset):
+        return {"int_to_str": dataset.features["ner_tags"].feature.int2str}
+
+    def _generate_document(self, example, int_to_str):
+        doc_id = example["id"]
+        tokens = example["tokens"]
+        ner_tags = [int_to_str(tag) for tag in example["ner_tags"]]
+
+        text, ner_spans = tokens_and_tags_to_text_and_labeled_spans(tokens=tokens, tags=ner_tags)
+
+        document = GermaNERDocument(text=text, id=doc_id)
+
+        for span in sorted(ner_spans, key=lambda span: span.start):
+            document.entities.append(span)
+
+        return document
