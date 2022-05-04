c99f72e9303c95fed7bea892bf9d435d6870ecd4
Arne Binder <arne.binder@dfki.de>
2022-05-04 11:50:56 +0200

    revert export Auto... classes; export core objects

--- src/pytorch_ie/__init__.py
+++ src/pytorch_ie/__init__.py
@@ -1,7 +1,5 @@
 # flake8: noqa
 
-from auto import AutoModel, AutoPipeline, AutoTaskModule
-
 from pytorch_ie.annotations import (
     BinaryRelation,
     Label,
@@ -13,9 +11,7 @@ from pytorch_ie.annotations import (
     MultiLabeledSpan,
     Span,
 )
-from pytorch_ie.core.document import Annotation, AnnotationList, Document, annotation_field
-from pytorch_ie.core.model import PyTorchIEModel
-from pytorch_ie.core.taskmodule import TaskModule
+from pytorch_ie.core import *
 from pytorch_ie.data import *
 from pytorch_ie.documents import TextDocument
 from pytorch_ie.pipeline import Pipeline
---
+++ src/pytorch_ie/core/__init__.py
@@ -0,0 +1,3 @@
+from .document import Annotation, AnnotationList, Document, annotation_field
+from .model import PyTorchIEModel
+from .taskmodule import TaskModule
