a1bc6b46dde71b97e1f7699570807de4125064f3
Arne Binder <arne.binder@dfki.de>
2022-05-05 18:52:37 +0200

    move PIEDatasetDict from data.datasets.__init__ to data.__init__ and rename to DatasetDict

--- src/pytorch_ie/data/__init__.py
+++ src/pytorch_ie/data/__init__.py
@@ -1,9 +1,16 @@
+from typing import Dict, Union
+
+from datasets import Split
+
 from .builder import GeneratorBasedBuilder
 from .dataset import Dataset
 from .dataset_formatter import DocumentFormatter
 
+DatasetDict = Dict[Union[str, Split], Dataset]
+
 __all__ = [
     "GeneratorBasedBuilder",
     "Dataset",
+    "DatasetDict",
     "DocumentFormatter",
 ]
