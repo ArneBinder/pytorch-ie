7c2f15adf31893a3cd874dac3e2a071e473ae3b9
Arne Binder <arne.binder@dfki.de>
2022-07-30 18:17:41 +0200

    fix iterating a document

--- src/pytorch_ie/core/document.py
+++ src/pytorch_ie/core/document.py
@@ -130,7 +130,7 @@ class Document(Mapping[str, Any]):
         return getattr(self, key)
 
     def __iter__(self):
-        return iter((field.name, getattr(self, field.name)) for field in self._annotation_fields)
+        return iter((field, getattr(self, field)) for field in self._annotation_fields)
 
     def __len__(self):
         return len(self._annotation_fields)
