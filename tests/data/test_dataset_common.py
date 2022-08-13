9514d0d61c295252ed66107bf7e2fc5f1dea5790
Arne Binder <arne.binder@dfki.de>
2022-08-13 16:34:01 +0200

    fix dataset tests (see https://github.com/huggingface/datasets/pull/4414)

--- tests/data/test_dataset_common.py
+++ tests/data/test_dataset_common.py
@@ -59,7 +59,7 @@ class LocalDatasetTest(parameterized.TestCase):
         builder_cls = self.dataset_tester.load_builder_class(dataset_name, is_local=True)
         name = builder_cls.BUILDER_CONFIGS[0].name if builder_cls.BUILDER_CONFIGS else None
         with tempfile.TemporaryDirectory() as tmp_cache_dir:
-            builder = builder_cls(name=name, cache_dir=tmp_cache_dir)
+            builder = builder_cls(config_name=name, cache_dir=tmp_cache_dir)
             self.assertIsInstance(builder, DatasetBuilder)
 
     def test_builder_configs(self, dataset_name):
