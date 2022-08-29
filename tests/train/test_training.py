9a2c151aef689f54b755f21547846685361e000a
Arne Binder <arne.binder@dfki.de>
2022-08-29 23:57:22 +0200

    add iterator case to test_training::test_transformer_token_classification and use real fixtures

--- tests/train/test_training.py
+++ tests/train/test_training.py
@@ -1,27 +1,60 @@
-from typing import List
+from typing import Iterator, List
 
 import pytest
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
 from pytorch_ie.core import Document, PyTorchIEModel, TaskModule
+from pytorch_ie.data.datamodules.datamodule import IterableTaskEncodingDataset
 from pytorch_ie.models import TransformerTokenClassificationModel
 from pytorch_ie.taskmodules import TransformerTokenClassificationTaskModule
 
+MODEL_NAME = "bert-base-cased"
 
-def _test_training(model: PyTorchIEModel, task_module: TaskModule, documents: List[Document]):
+
+@pytest.fixture
+def prepared_taskmodule(documents):
+    taskmodule = TransformerTokenClassificationTaskModule(
+        tokenizer_name_or_path=MODEL_NAME,
+        max_length=128,
+    )
+    taskmodule.prepare(documents)
+    return taskmodule
+
+
+@pytest.fixture
+def model(prepared_taskmodule):
+    model = TransformerTokenClassificationModel(
+        model_name_or_path=MODEL_NAME,
+        num_classes=len(prepared_taskmodule.label_to_id),
+        learning_rate=1e-4,
+    )
+    return model
+
+
+@pytest.mark.parametrize("encode_batch_size", [None, 2])
+def test_transformer_token_classification(
+    model, prepared_taskmodule, documents, encode_batch_size
+):
     pl.seed_everything(42)
 
     num_epochs = 1
     batch_size = 32
 
-    train_dataset = task_module.encode(documents, encode_target=True)
+    train_dataset = prepared_taskmodule.encode(
+        documents, encode_target=True, batch_size=encode_batch_size
+    )
 
+    if isinstance(train_dataset, Iterator):
+        train_dataset = IterableTaskEncodingDataset(train_dataset)
+        shuffle = False
+    else:
+        shuffle = True
     train_dataloader = DataLoader(
         train_dataset,  # type: ignore
         batch_size=batch_size,
-        shuffle=True,
-        collate_fn=task_module.collate,
+        shuffle=shuffle,
+        collate_fn=prepared_taskmodule.collate,
     )
 
     trainer = pl.Trainer(
@@ -32,22 +65,3 @@ def _test_training(model: PyTorchIEModel, task_module: TaskModule, documents: Li
         precision=32,
     )
     trainer.fit(model, train_dataloader)
-
-
-@pytest.mark.slow
-def test_transformer_token_classification(documents):
-    model_name = "bert-base-cased"
-
-    task_module = TransformerTokenClassificationTaskModule(
-        tokenizer_name_or_path=model_name,
-        max_length=128,
-    )
-
-    task_module.prepare(documents)
-    model = TransformerTokenClassificationModel(
-        model_name_or_path=model_name,
-        num_classes=len(task_module.label_to_id),
-        learning_rate=1e-4,
-    )
-
-    _test_training(model=model, task_module=task_module, documents=documents)
