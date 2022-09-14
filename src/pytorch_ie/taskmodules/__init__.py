4e2f108de7b057ad7bcd2fc3fc7fd4fa2e8fdb95
Arne Binder <arne.binder@dfki.de>
2022-09-14 11:45:16 +0200

    fix export SimpleTransformerTextClassificationTaskModule

--- src/pytorch_ie/taskmodules/__init__.py
+++ src/pytorch_ie/taskmodules/__init__.py
@@ -1,3 +1,4 @@
+from .simple_transformer_text_classification import SimpleTransformerTextClassificationTaskModule
 from .transformer_re_text_classification import TransformerRETextClassificationTaskModule
 from .transformer_seq2seq import TransformerSeq2SeqTaskModule
 from .transformer_span_classification import TransformerSpanClassificationTaskModule
