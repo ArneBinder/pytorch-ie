d88560ba415fd6baebda701e6bf88f6ce2e3ea7f
Arne Binder <arne.binder@dfki.de>
2023-07-19 15:44:20 +0200

    add (helper) document types: WithMetadata, WithTokens, WithText, TextBasedDocument, TokenBasedDocument

--- src/pytorch_ie/documents.py
+++ src/pytorch_ie/documents.py
@@ -1,11 +1,36 @@
 import dataclasses
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Tuple
+
+from typing_extensions import TypeAlias
 
 from pytorch_ie.core import Document
 
 
 @dataclasses.dataclass
-class TextDocument(Document):
-    text: str
+class WithMetadata:
     id: Optional[str] = None
     metadata: Dict[str, Any] = dataclasses.field(default_factory=dict)
+
+
+@dataclasses.dataclass
+class WithTokens:
+    tokens: Tuple[str, ...]
+
+
+@dataclasses.dataclass
+class WithText:
+    text: str
+
+
+@dataclasses.dataclass
+class TextBasedDocument(WithMetadata, WithText, Document):
+    pass
+
+
+@dataclasses.dataclass
+class TokenBasedDocument(WithMetadata, WithTokens, Document):
+    pass
+
+
+# backwards compatibility
+TextDocument: TypeAlias = TextBasedDocument
