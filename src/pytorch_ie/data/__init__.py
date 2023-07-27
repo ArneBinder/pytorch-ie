92c47cad0868479070c176153221df2b65654b10
Arne Binder <arne.binder@dfki.de>
2023-07-28 01:19:09 +0200

    reset commits b8128cace018dfe50ea9233174f2b86df60d83f6 and 99176e0fc744cc849c882cef1712d131e8d7d1d2

--- src/pytorch_ie/data/__init__.py
+++ src/pytorch_ie/data/__init__.py
@@ -1,7 +1,12 @@
+from typing import Dict, Union
+
+from datasets import Split
+
 from .builder import GeneratorBasedBuilder
 from .dataset import Dataset, IterableDataset
-from .dataset_dict import DatasetDict
 from .dataset_formatter import DocumentFormatter
+
+DatasetDict = Dict[Union[str, Split], Dataset]
 
 __all__ = [
     "GeneratorBasedBuilder",
