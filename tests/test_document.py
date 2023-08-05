e22ffd1b49e4b1c2be1c488dd8adc420ce1ca69b
Arne Binder <arne.binder@dfki.de>
2023-08-05 22:23:16 +0200

    check that it works also for nested annotations

--- tests/test_document.py
+++ tests/test_document.py
@@ -604,6 +604,15 @@ def test_annotation_compare():
     # ... but, again, the id is different.
     assert LabeledSpan(0, 1, "test")._id != LabeledSpan(0, 1, "test", score=0.9)._id
 
+    # works also for nested annotations
+    e1 = LabeledSpan(0, 1, "test")
+    e2 = LabeledSpan(0, 1, "test", score=0.9)
+    e3 = LabeledSpan(3, 4, "test", score=0.5)
+    assert e1 == e2
+    r1 = BinaryRelation(e1, e3, "test")
+    r2 = BinaryRelation(e2, e3, "test")
+    assert r1 == r2
+
 
 def test_revert_annotation_graph():
     @dataclasses.dataclass
