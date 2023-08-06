7d43fd30733909bf5dad22b04f2ef83c84a4c031
Arne Binder <arne.binder@dfki.de>
2023-08-06 18:19:50 +0200

    add more tests cases

--- tests/core/test_statistic.py
+++ tests/core/test_statistic.py
@@ -1,18 +1,16 @@
 import dataclasses
-from collections import defaultdict
-from functools import partial
-from typing import Callable, Dict, List, Tuple, Union
 
 import pytest
-from typing_extensions import TypeAlias
 
 from pytorch_ie import DatasetDict
 from pytorch_ie.annotations import LabeledSpan
-from pytorch_ie.core import AnnotationList, Document, annotation_field
+from pytorch_ie.core import AnnotationList, annotation_field
 from pytorch_ie.core.statistic import flatten_dict
 from pytorch_ie.documents import TextBasedDocument
 from pytorch_ie.metrics.statistics import (
     DocumentFieldLengthCounter,
+    DocumentSpanLengthCounter,
+    DocumentSubFieldLengthCounter,
     DocumentTokenCounter,
     DummyCounter,
     LabelCounter,
@@ -65,6 +63,32 @@ def test_prepare_data(dataset):
         ("validation",): [65, 17, 187],
     }
 
+    statistic = DocumentSpanLengthCounter(field="entities")
+    values_nested = statistic(dataset)
+    prepared_data = flatten_dict(values_nested)
+    assert prepared_data == {
+        ("train", "ORG"): [2],
+        ("train", "MISC"): [6, 7],
+        ("train", "PER"): [15],
+        ("train", "LOC"): [8],
+        ("test", "LOC"): [5, 6, 20],
+        ("test", "PER"): [5, 11],
+        ("validation", "ORG"): [14, 14, 8],
+        ("validation", "LOC"): [6],
+        ("validation", "MISC"): [11],
+        ("validation", "PER"): [12],
+    }
+
+    # this is not super useful, we just collect teh lengths of the labels, but it is enough to test the code
+    statistic = DocumentSubFieldLengthCounter(field="entities", subfield="label")
+    values_nested = statistic(dataset)
+    prepared_data = flatten_dict(values_nested)
+    assert prepared_data == {
+        ("train",): [3, 4, 4, 3, 3],
+        ("test",): [3, 3, 3, 3, 3],
+        ("validation",): [3, 3, 4, 3, 3, 3],
+    }
+
 
 @pytest.mark.slow
 def test_prepare_data_tokenize(dataset):
@@ -77,77 +101,4 @@ def test_prepare_data_tokenize(dataset):
         ("train",): [9, 2, 6],
         ("test",): [12, 4, 12],
         ("validation",): [11, 6, 38],
-    }
-
-
-def label_counter(doc: Document, field: str) -> Dict[str, int]:
-    field_obj = getattr(doc, field)
-    counts: Dict[str, int] = defaultdict(lambda: 1)
-    for elem in field_obj:
-        counts[elem.label] += 1
-    return dict(counts)
-
-
-def document_field_length_collector(doc: Document, field: str) -> int:
-    field_obj = getattr(doc, field)
-    return len(field_obj)
-
-
-# The metric should return a single int or float or a list of such values ...
-BaseType: TypeAlias = Union[int, float]
-ResultTerminal: TypeAlias = Union[BaseType, List[BaseType]]
-# ... or such entries nested arbitrarily deep inside dictionaries.
-ResultDict: TypeAlias = Dict[str, Union[ResultTerminal, "ResultDict"]]
-
-
-def prepare_data(
-    dataset: DatasetDict,
-    metric: Callable[[Document], Union[ResultTerminal, ResultDict]],
-) -> Dict[Tuple[str, ...], List[BaseType]]:
-    stats = defaultdict(list)
-    for s_name, split in dataset.items():
-        for doc in split:
-            metric_result = metric(doc)
-            if isinstance(metric_result, dict):
-                metric_result_flat = flatten_dict(metric_result)
-                for k, v in metric_result_flat.items():
-                    if isinstance(v, list):
-                        stats[(s_name,) + k].extend(v)
-                    else:
-                        stats[(s_name,) + k].append(v)
-            else:
-                if isinstance(metric_result, list):
-                    stats[(s_name,)].extend(metric_result)
-                else:
-                    stats[(s_name,)].append(metric_result)
-    return dict(stats)
-
-
-def test_prepare_data_simple(dataset):
-    prepared_data = prepare_data(dataset=dataset, metric=lambda doc: 1)
-    assert prepared_data == {
-        ("train",): [1, 1, 1],
-        ("test",): [1, 1, 1],
-        ("validation",): [1, 1, 1],
-    }
-    prepared_data = prepare_data(dataset=dataset, metric=partial(label_counter, field="entities"))
-    assert prepared_data == {
-        ("train", "ORG"): [2],
-        ("train", "MISC"): [3],
-        ("train", "PER"): [2],
-        ("train", "LOC"): [2],
-        ("test", "LOC"): [2, 3],
-        ("test", "PER"): [2, 2],
-        ("validation", "ORG"): [2, 3],
-        ("validation", "LOC"): [2],
-        ("validation", "MISC"): [2],
-        ("validation", "PER"): [2],
-    }
-    prepared_data = prepare_data(
-        dataset=dataset, metric=partial(document_field_length_collector, field="text")
-    )
-    assert prepared_data == {
-        ("train",): [48, 15, 19],
-        ("test",): [57, 11, 40],
-        ("validation",): [65, 17, 187],
     }
