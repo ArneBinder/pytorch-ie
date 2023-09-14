8682c2a6e2e292516f21c115d32c7dc118c9b869
Arne Binder <arne.binder@dfki.de>
2023-09-14 14:32:31 +0200

    implement RequiresDocumentTypeMixin

--- src/pytorch_ie/core/__init__.py
+++ src/pytorch_ie/core/__init__.py
@@ -1,5 +1,6 @@
 from .document import Annotation, AnnotationList, Document, annotation_field
 from .metric import DocumentMetric
 from .model import PyTorchIEModel
+from .module_mixins import RequiresDocumentTypeMixin
 from .statistic import DocumentStatistic
 from .taskmodule import TaskEncoding, TaskModule
