01d46786db503ae60a2115299d3940e58cdbcfbf
Arne Binder <arne.binder@dfki.de>
2023-11-14 12:28:25 +0100

    define exported taskmodules

--- src/pytorch_ie/taskmodules/__init__.py
+++ src/pytorch_ie/taskmodules/__init__.py
@@ -4,3 +4,12 @@ from .transformer_seq2seq import TransformerSeq2SeqTaskModule
 from .transformer_span_classification import TransformerSpanClassificationTaskModule
 from .transformer_text_classification import TransformerTextClassificationTaskModule
 from .transformer_token_classification import TransformerTokenClassificationTaskModule
+
+__all__ = [
+    "SimpleTransformerTextClassificationTaskModule",
+    "TransformerRETextClassificationTaskModule",
+    "TransformerSeq2SeqTaskModule",
+    "TransformerSpanClassificationTaskModule",
+    "TransformerTextClassificationTaskModule",
+    "TransformerTokenClassificationTaskModule",
+]
