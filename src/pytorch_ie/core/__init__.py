63ddddf5d95b43cc0d4d354fe92e1dc8c00c1436
Arne Binder <arne.binder@dfki.de>
2023-11-26 06:11:18 +0100

    add dataset mixins

--- src/pytorch_ie/core/__init__.py
+++ src/pytorch_ie/core/__init__.py
@@ -1,7 +1,14 @@
 from .document import Annotation, AnnotationLayer, Document, annotation_field
 from .metric import DocumentMetric
 from .model import PyTorchIEModel
-from .module_mixins import WithDocumentTypeMixin
+from .module_mixins import (
+    EnterDatasetDictMixin,
+    EnterDatasetMixin,
+    ExitDatasetDictMixin,
+    ExitDatasetMixin,
+    PreparableMixin,
+    WithDocumentTypeMixin,
+)
 from .statistic import DocumentStatistic
 from .taskmodule import TaskEncoding, TaskModule
 
