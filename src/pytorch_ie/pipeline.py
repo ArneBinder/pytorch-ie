4ca830a6013368c09cc85be0faf2a4e05d38c121
Arne Binder <arne.binder@dfki.de>
2025-01-13 22:35:17 +0100

    fix documentation

--- src/pytorch_ie/pipeline.py
+++ src/pytorch_ie/pipeline.py
@@ -339,7 +339,7 @@ class Pipeline:
                 run. If set to :obj:`True`, only the first two model inputs will be processed.
             half_precision_ops (:obj:`bool`, `optional`, defaults to :obj:`False`): Whether or not to use half
                 precision operations. If set to :obj:`True`, the model will be run with half precision operations
-                via :obj:`torch.cuda.amp.autocast`.
+                via :obj:`torch.autocast`.
             batch_size (:obj:`int`, `optional`, defaults to :obj:`1`): The batch size to use for the dataloader. If not
                 provided, a batch size of 1 will be used.
             num_workers (:obj:`int`, `optional`, defaults to :obj:`8`): The number of workers to use for the dataloader.
