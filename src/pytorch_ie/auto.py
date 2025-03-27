d4cff8be2543938161c3e5c11408e1600bb15598
Arne Binder <arne.binder@dfki.de>
2025-03-28 00:47:48 +0100

    do not call taskmodule.post_prepare() multiple times and remove duplicated code

--- src/pytorch_ie/auto.py
+++ src/pytorch_ie/auto.py
@@ -15,51 +15,6 @@ from pytorch_ie.pipeline import Pipeline
 
 
 class AutoModel(PieModelHFHubMixin):
-    @classmethod
-    def _from_pretrained(
-        cls,
-        *,
-        model_id: str,
-        revision: Optional[str],
-        cache_dir: Optional[Union[str, Path]],
-        force_download: bool,
-        proxies: Optional[Dict],
-        resume_download: bool,
-        local_files_only: bool,
-        token: Union[str, bool, None],
-        map_location: str = "cpu",
-        strict: bool = False,
-        config: Optional[dict] = None,
-        config_override: Optional[TOverride] = None,
-        **model_kwargs,
-    ) -> PyTorchIEModel:
-        """
-        Overwrite this method in case you wish to initialize your model in a different way.
-        """
-
-        config = (config or {}).copy()
-        dict_update_nested(config, model_kwargs, override=config_override)
-        model = cls.from_config(config, **model_kwargs)
-
-        """Load Pytorch pretrained weights and return the loaded model."""
-        if os.path.isdir(model_id):
-            model_file = os.path.join(model_id, model.weights_file_name)
-        else:
-            model_file = hf_hub_download(
-                repo_id=model_id,
-                filename=model.weights_file_name,
-                revision=revision,
-                cache_dir=cache_dir,
-                force_download=force_download,
-                proxies=proxies,
-                resume_download=resume_download,
-                token=token,
-                local_files_only=local_files_only,
-            )
-
-        model.load_model_file(model_file, map_location=map_location, strict=strict)
-
-        return model
 
     @classmethod
     def from_config(cls, config: dict, **kwargs) -> PyTorchIEModel:
@@ -71,29 +26,6 @@ class AutoModel(PieModelHFHubMixin):
 
 
 class AutoTaskModule(PieTaskModuleHFHubMixin):
-    @classmethod
-    def _from_pretrained(  # type: ignore
-        cls,
-        *,
-        model_id: str,
-        revision: Optional[str],
-        cache_dir: Optional[Union[str, Path]],
-        force_download: bool,
-        proxies: Optional[Dict],
-        resume_download: bool,
-        local_files_only: bool,
-        token: Union[str, bool, None],
-        map_location: str = "cpu",
-        strict: bool = False,
-        config: Optional[dict] = None,
-        config_override: Optional[TOverride] = None,
-        **taskmodule_kwargs,
-    ) -> TaskModule:
-        config = (config or {}).copy()
-        dict_update_nested(config, taskmodule_kwargs)
-        taskmodule = cls.from_config(config)
-        taskmodule.post_prepare()
-        return taskmodule
 
     @classmethod
     def from_config(cls, config: dict, **kwargs) -> TaskModule:  # type: ignore
