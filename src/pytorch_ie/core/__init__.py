81d34349e89e70e35193daa8076e8b6eedf45b2b
Arne Binder <arne.binder@dfki.de>
2025-04-02 19:50:57 +0200

    use pie-core in a non-breaking approach (re-export at the original locations)

--- src/pytorch_ie/core/__init__.py
+++ src/pytorch_ie/core/__init__.py
@@ -1,7 +1,10 @@
-from .document import Annotation, AnnotationLayer, Document, annotation_field
-from .metric import DocumentMetric
-from .model import PyTorchIEModel
-from .module_mixins import (
+import sys
+
+import pie_core
+from pie_core.document import Annotation, AnnotationLayer, Document, annotation_field
+from pie_core.metric import DocumentMetric
+from pie_core.model import PyTorchIEModel
+from pie_core.module_mixins import (
     EnterDatasetDictMixin,
     EnterDatasetMixin,
     ExitDatasetDictMixin,
@@ -9,8 +12,13 @@ from .module_mixins import (
     PreparableMixin,
     WithDocumentTypeMixin,
 )
-from .statistic import DocumentStatistic
-from .taskmodule import TaskEncoding, TaskModule
+from pie_core.statistic import DocumentStatistic
+from pie_core.taskmodule import TaskEncoding, TaskModule
+
+submodules = ["document", "taskmodule", "metric", "model", "statistic"]
+for sub in submodules:
+    module = getattr(pie_core, sub)
+    sys.modules[f"{__name__}.{sub}"] = module
 
 # backwards compatibility
 AnnotationList = AnnotationLayer
