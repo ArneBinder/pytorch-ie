8423ab5cbcd7f5aae933d2585b808938d7896760
Arne Binder <arne.binder@dfki.de>
2025-04-02 20:16:20 +0200

    integrate from_pretrained from AutoPipeline into Pipeline

--- src/pytorch_ie/pipeline.py
+++ src/pytorch_ie/pipeline.py
@@ -8,6 +8,7 @@ from typing import Any, Dict, List, MutableSequence, Optional, Sequence, Tuple, 
 import torch
 import tqdm
 from packaging import version
+from pie_core import AutoModel, AutoTaskModule
 from torch import Tensor
 from torch.utils.data import DataLoader
 from transformers.utils import ModelOutput
@@ -108,6 +109,54 @@ class Pipeline:
         self.model.save_pretrained(save_directory)
 
         self.taskmodule.save_pretrained(save_directory)
+
+    @staticmethod
+    def from_pretrained(
+        pretrained_model_name_or_path: str,
+        force_download: bool = False,
+        resume_download: bool = False,
+        proxies: Optional[Dict] = None,
+        use_auth_token: Optional[str] = None,
+        cache_dir: Optional[str] = None,
+        local_files_only: bool = False,
+        taskmodule_kwargs: Optional[Dict[str, Any]] = None,
+        model_kwargs: Optional[Dict[str, Any]] = None,
+        device: int = -1,
+        binary_output: bool = False,
+        **kwargs,
+    ) -> "Pipeline":
+        taskmodule_kwargs = taskmodule_kwargs or {}
+        model_kwargs = model_kwargs or {}
+
+        taskmodule = AutoTaskModule.from_pretrained(
+            pretrained_model_name_or_path=pretrained_model_name_or_path,
+            force_download=force_download,
+            resume_download=resume_download,
+            proxies=proxies,
+            use_auth_token=use_auth_token,
+            cache_dir=cache_dir,
+            local_files_only=local_files_only,
+            **taskmodule_kwargs,
+        )
+
+        model = AutoModel.from_pretrained(
+            pretrained_model_name_or_path=pretrained_model_name_or_path,
+            force_download=force_download,
+            resume_download=resume_download,
+            proxies=proxies,
+            use_auth_token=use_auth_token,
+            cache_dir=cache_dir,
+            local_files_only=local_files_only,
+            **model_kwargs,
+        )
+
+        return Pipeline(
+            taskmodule=taskmodule,
+            model=model,
+            device=device,
+            binary_output=binary_output,
+            **kwargs,
+        )
 
     def transform(self, X):
         """
