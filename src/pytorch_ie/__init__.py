04f33cd8bc0edfca6e52a963af5946b0b7d30309
Arne Binder <arne.binder@dfki.de>
2025-04-03 16:36:03 +0200

    adjusts for changes in pie-core

--- src/pytorch_ie/__init__.py
+++ src/pytorch_ie/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 
-from pytorch_ie.auto import AutoModel, AutoPipeline, AutoTaskModule
+from pytorch_ie.auto import AutoPipeline
 from pytorch_ie.core import *
 from pytorch_ie.pipeline import Pipeline
