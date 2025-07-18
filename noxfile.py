655fd5b203ae27328f60593785927e266df7a758
Arne Binder <arne.binder@dfki.de>
2025-07-18 20:25:00 +0200

    fix mypy setup in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -144,7 +144,7 @@ def safety(session: Session) -> None:
 @session(python=python_versions)
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
-    args = session.posargs or ["src", "tests", "docs/conf.py"]
+    args = session.posargs or ["src", "tests", "examples", "docs/conf.py"]
     session.install(".")
     session.install("mypy", "pytest")
     session.run("mypy", *args)
