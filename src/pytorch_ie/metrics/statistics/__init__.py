2c0c2a3c2dc5c5e52739c9590d10589e9fb44e17
Arne Binder <arne.binder@dfki.de>
2025-07-19 18:13:18 +0200

    remove metrics and statistics (except TokenCountCollector), but import them from `pie-modules`; fix test_confusion_matrix

---
+++ src/pytorch_ie/metrics/statistics/__init__.py
@@ -0,0 +1,8 @@
+from pie_modules.metrics.statistics import (
+    DummyCollector,
+    FieldLengthCollector,
+    LabelCountCollector,
+    SubFieldLengthCollector,
+)
+
+from .token_count_collector import TokenCountCollector
---
+++ src/pytorch_ie/metrics/statistics/token_count_collector.py
@@ -0,0 +1,40 @@
+import logging
+from typing import Any, Dict, Optional, Type, Union
+
+from pie_core import Document, DocumentStatistic
+from transformers import AutoTokenizer, PreTrainedTokenizer
+
+from pytorch_ie.documents import TextBasedDocument
+
+logger = logging.getLogger(__name__)
+
+
+class TokenCountCollector(DocumentStatistic):
+    """Collects the token count of a field when tokenizing its content with a Huggingface
+    tokenizer.
+
+    The content of the field should be a string.
+    """
+
+    def __init__(
+        self,
+        tokenizer: Union[str, PreTrainedTokenizer],
+        text_field: str = "text",
+        tokenizer_kwargs: Optional[Dict[str, Any]] = None,
+        document_type: Optional[Type[Document]] = None,
+        **kwargs,
+    ):
+        if document_type is None and text_field == "text":
+            document_type = TextBasedDocument
+        super().__init__(document_type=document_type, **kwargs)
+        self.tokenizer = (
+            AutoTokenizer.from_pretrained(tokenizer) if isinstance(tokenizer, str) else tokenizer
+        )
+        self.tokenizer_kwargs = tokenizer_kwargs or {}
+        self.text_field = text_field
+
+    def _collect(self, doc: Document) -> int:
+        text = getattr(doc, self.text_field)
+        encodings = self.tokenizer(text, **self.tokenizer_kwargs)
+        tokens = encodings.tokens()
+        return len(tokens)
