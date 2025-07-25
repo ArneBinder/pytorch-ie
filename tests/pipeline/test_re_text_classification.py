a8d24c1528299d5c12fe6bbd806b36885aaf2c36
Arne Binder <arne.binder@dfki.de>
2025-07-25 15:30:15 +0200

    re-add branch to fix local tests

--- tests/pipeline/test_re_text_classification.py
+++ tests/pipeline/test_re_text_classification.py
@@ -85,14 +85,17 @@ def test_re_text_classification(use_auto, half_precision_model, half_precision_o
     # Below scores for torch < 2.6 were obtained with:
     #  - torch==2.3.0, pytorch-lightning==2.2.5, and transformers==4.41.1
     # The scores for torch >= 2.6 were obtained with:
-    #  - torch==2.7.1, pytorch-lightning==2.4.0, and transformers==4.48.3.
+    #  - torch==2.7.1, pytorch-lightning==2.5.2, and transformers==4.48.3.
     if not half_precision_model and not half_precision_ops:
         assert scores == pytest.approx(
             [0.5339038372039795, 0.3984701931476593, 0.5520647764205933], abs=1e-6
         )
         assert half_precision_warning not in caplog.messages
     elif not half_precision_model and half_precision_ops:
-        assert scores == pytest.approx([0.53125, 0.396484375, 0.5546875], abs=1e-6)
+        if Version(version("torch")) < Version("2.6"):
+            assert scores == pytest.approx([0.53125, 0.39453125, 0.5546875], abs=1e-6)
+        else:
+            assert scores == pytest.approx([0.53125, 0.396484375, 0.5546875], abs=1e-6)
         assert half_precision_warning not in caplog.messages
     elif half_precision_model and not half_precision_ops:
         if Version(version("torch")) < Version("2.6"):
