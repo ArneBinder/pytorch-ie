bf050b86b761df0ca8f9165c46dfd6898fadfdd4
Arne Binder <arne.binder@dfki.de>
2025-08-02 17:36:00 +0200

    re-add abs=1e-6 for full precision

--- tests/pipeline/test_re_text_classification.py
+++ tests/pipeline/test_re_text_classification.py
@@ -82,10 +82,10 @@ def test_re_text_classification(use_auto, half_precision_model, half_precision_o
     # for the taskmodule, so the argument markers are not correct.
     # Below scores were obtained with dependencies from poetry.lock on local machine.
     if not half_precision_model and not half_precision_ops:
-        # we expect exact scores when no half precision is used
+        # we expect high precision matches if no half precision is used
         # (i.e., no autocast on forward pass and model is not cast to half precision)
         assert scores == pytest.approx(
-            [0.5339038372039795, 0.3984701931476593, 0.5520647764205933]
+            [0.5339038372039795, 0.3984701931476593, 0.5520647764205933], abs=1e-6
         )
         assert half_precision_warning not in caplog.messages
     elif not half_precision_model and half_precision_ops:
