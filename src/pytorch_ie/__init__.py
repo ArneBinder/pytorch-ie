5952dcf9d1ef4eca06163abb8f73607ef3ffa3d2
Arne Binder <arne.binder@dfki.de>
2025-09-24 20:02:35 +0200

    changes:  - `core` just re-exports everything from `pie_core`  - `metrics` just re-exports everything from `pie_documents.metrics`  - `annotations` just re-exports everything from `pie_documents.annotations`  - `documents` just re-exports everything from `pie_documents.documents`  - remove `utils.hydra`  - remove `auto`

--- src/pytorch_ie/__init__.py
+++ src/pytorch_ie/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 
-from pytorch_ie.auto import AutoModel, AutoTaskModule
 from pytorch_ie.core import *
 from pytorch_ie.datamodule import PieDataModule
 from pytorch_ie.dataset import IterableTaskEncodingDataset, TaskEncodingDataset
+from pytorch_ie.model import PyTorchIEModel
 from pytorch_ie.pipeline import PyTorchIEPipeline
--- src/pytorch_ie/utils/hydra.py
+++
@@ -1,7 +0,0 @@
-# backwards compatibility
-from pie_core.utils.hydra import (
-    InstantiationException,
-    resolve_optional_document_type,
-    resolve_target,
-    serialize_document_type,
-)
