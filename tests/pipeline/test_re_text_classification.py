334e433974ae2c44c409c71a3c98695fd32c1580
Arne Binder <arne.binder@dfki.de>
2025-09-24 16:46:29 +0200

    just fix spelling to trigger ci

--- tests/pipeline/test_re_text_classification.py
+++ tests/pipeline/test_re_text_classification.py
@@ -82,7 +82,7 @@ def test_re_text_classification(use_auto, half_precision_model, half_precision_o
     # for the taskmodule, so the argument markers are not correct.
     # Below scores were obtained with dependencies from poetry.lock on local machine.
     if not half_precision_model and not half_precision_ops:
-        # we low tolerance if no half precision is used
+        # we use low tolerance if no half precision is used
         # (i.e., no autocast on forward pass and model is not cast to half precision)
         assert scores == pytest.approx(
             [0.5339038372039795, 0.3984701931476593, 0.5520647764205933], abs=1e-6
